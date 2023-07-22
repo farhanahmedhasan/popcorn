424321a282fe7517c9a47b1abc9aa82614f1e155
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-07-22 15:37:19 +0600

    creating a reuseable starRating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import StarRating from "./components/StarRating.jsx";
+// import App from './App.jsx'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={10}/>
   </React.StrictMode>,
 )
