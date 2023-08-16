19c72a4de8b84552d58ea1b7a80d8639872daaf1
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-08-16 18:01:45 +0600

    made starRating component flexible

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -10,49 +10,66 @@ const starContainerStyle = {
     display: 'flex',
 }
 
-const textStyle = {
-    lineHeight : '1',
-    margin : '0'
-}
-
-export default function StarRating({maxRating= 5}) {
-    const [rating, setRating] = useState(0)
+export default function StarRating({
+    maxRating= 5,
+    starColor= "#fcc419",
+    textColor="#fcc419",
+    size=48,
+    messages=[],
+    className="",
+    defaultRating = 0,
+    onSetRating,
+}) {
+    const [rating, setRating] = useState(defaultRating)
     const [tempRating, setTempRating] = useState(0)
 
     function handleRating(rate) {
         setRating(rate)
+        if (onSetRating) onSetRating(rate)
+    }
+
+    const textStyle = {
+        lineHeight : '1',
+        margin : '0',
+        fontSize:`${size/1.5}px`,
+        color: textColor,
     }
 
     return (
-        <div style={containerStyle}>
+        <div style={containerStyle} className={className}>
             <div style={starContainerStyle}>
                 {Array.from({length:maxRating}, (_,i)=>(
                     <Star key={i} isFull={tempRating ? tempRating >= i+1 : rating >= i+1} onRate={()=> handleRating(i+1)}
-                          onHoverIn={()=> setTempRating(i+1)} onHoverOut={()=> setTempRating(0)}/>
+                          onHoverIn={()=> setTempRating(i+1)} onHoverOut={()=> setTempRating(0)}
+                          starColor={starColor} size={size}
+                    />
                 ))}
             </div>
 
-            <p style={textStyle}> {tempRating || rating || ""} </p>
+            <p style={textStyle}>
+                {messages.length === maxRating ? messages[tempRating ? tempRating - 1 : rating - 1]
+                : tempRating || rating || ""}
+            </p>
         </div>
     )
 }
 
-const starStyle = {
-    height: "48px",
-    width: "48px",
-    cursor: "pointer",
-    display: "block"
-}
+function Star({isFull, onRate, onHoverIn, onHoverOut, starColor, size}){
+    const starStyle = {
+        height: `${size}px`,
+        width: `${size}px`,
+        cursor: "pointer",
+        display: "block"
+    }
 
-function Star({isFull, onRate, onHoverIn, onHoverOut}){
     return (
         <span role="button" style={starStyle} onClick={onRate} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
             {isFull ? (
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
-                    fill="#000"
-                    stroke="#000"
+                    fill={starColor}
+                    stroke={starColor}
                 >
                     <path
                         d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -63,7 +80,7 @@ function Star({isFull, onRate, onHoverIn, onHoverOut}){
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke="#000"
+                    stroke={starColor}
                 >
                     <path
                         strokeLinecap="round"
