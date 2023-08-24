733be3a8faedee14b997d8b5b77a970dfa3e7b01
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-08-24 12:20:48 +0600

    fixed/fractions-after-int

--- src/components/main/WatchedMoviesSummary.jsx
+++ src/components/main/WatchedMoviesSummary.jsx
@@ -2,9 +2,9 @@ const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function WatchedMoviesSummary({watched}) {
-    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-    const avgUserRating = average(watched.map((movie) => movie.userRating));
-    const avgRuntime = average(watched.map((movie) => movie.runtime));
+    const avgImdbRating = average(watched.map((movie) => movie.imdbRating)).toFixed(2);
+    const avgUserRating = average(watched.map((movie) => movie.userRating)).toFixed(2);
+    const avgRuntime = average(watched.map((movie) => movie.runtime)).toFixed(2);
 
     return(
         <div className="summary">
